c082384148f393ae4d994eaa1bee7315d533be58
franciscozdo <zdobyl.frank@gmail.com>
2023-09-01 19:23:29 +0200

    Add minor plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,9 +9,17 @@ return {
   },
 
   'lewis6991/gitsigns.nvim',
+  'ethanholz/nvim-lastplace',
 
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+  },
+
+  {
+    'nmac427/guess-indent.nvim',
+    config = function()
+      require('guess-indent').setup()
+    end,
   },
 }
