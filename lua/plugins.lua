70f1963787f5cba442debb520b56411d305d342a
franciscozdo <zdobyl.frank@gmail.com>
2023-09-04 06:40:14 +0200

    Update nvim-treesitter installation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,7 +28,9 @@ return {
 
   {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    build = function ()
+      require('nvim-treesitter.install').update({with_sync = true})
+    end,
   },
 
   {
